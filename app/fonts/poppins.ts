ae27312c6ad9e8e42bddd717c3e54e593d50d2c5
Robert <robert.walker6580@gmail.com>
2024-11-24 18:50:21 -0800

    create basic layout

---
+++ app/fonts/poppins.ts
@@ -0,0 +1,97 @@
+import localFont from 'next/font/local';
+
+export const poppins = localFont({
+  src: [
+    {
+      path: 'poppins/poppins-v21-latin-regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: 'poppins/poppins-v21-latin-italic.woff2',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: 'poppins/poppins-v21-latin-100.woff2',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: 'poppins/poppins-v21-latin-100italic.woff2',
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: 'poppins/poppins-v21-latin-200.woff2',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: 'poppins/poppins-v21-latin-200italic.woff2',
+      weight: '200',
+      style: 'italic',
+    },
+    {
+      path: 'poppins/poppins-v21-latin-300.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: 'poppins/poppins-v21-latin-300italic.woff2',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: 'poppins/poppins-v21-latin-500.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: 'poppins/poppins-v21-latin-500italic.woff2',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: 'poppins/poppins-v21-latin-600.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: 'poppins/poppins-v21-latin-600italic.woff2',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: 'poppins/poppins-v21-latin-700.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: 'poppins/poppins-v21-latin-700italic.woff2',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: 'poppins/poppins-v21-latin-800.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: 'poppins/poppins-v21-latin-800italic.woff2',
+      weight: '800',
+      style: 'italic',
+    },
+    {
+      path: 'poppins/poppins-v21-latin-900.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: 'poppins/poppins-v21-latin-900italic.woff2',
+      weight: '900',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-poppins',
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,46 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
+  darkMode: ['class'],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        neutral: '#ffffff',
+        primary: '#2a85ff',
+        primaryDeep: '#0069f6',
+        primaryMild: '#4996ff',
+        primarySubtle: '#2a85ff1a',
+        error: '#ff6a55',
+        errorSubtle: '#ff6a551a',
+        success: '#10b981',
+        successSubtle: '#05eb7624',
+        info: '#2a85ff',
+        infoSubtle: '#2a85ff1a',
+        warning: '#f59e0b',
+        warningSubtle: '#ffd40045',
+        gray: {
+          50: '#fafafa',
+          100: '#f5f5f5',
+          200: '#e5e5e5',
+          300: '#d4d4d4',
+          400: '#a3a3a3',
+          500: '#737373',
+          600: '#525252',
+          700: '#404040',
+          800: '#262626',
+          900: '#171717',
+          950: '#0a0a0a',
+        },
+      },
+      fontFamily: {
+        poppins: ['var(--font-poppins)'],
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
